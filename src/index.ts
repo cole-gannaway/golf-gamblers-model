171b962ebfdd4f2a5a7861d7d729c96eeb2414b7
Cole Gannaway <43042842+cole-gannaway@users.noreply.github.com>
2021-04-09 19:31:43 -0400

    Bugfix export from index correctly

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,13 @@ import * as FirebaseFirestore from '@google-cloud/firestore';
 
 export { Bet, BetType } from './bet';
 export { Course, Location } from './course';
-export { Event, EventMetaData } from './event';
+export {
+  Event,
+  EventConfiguration,
+  EventPrivateData,
+  EventAdmins,
+  EventUsers,
+} from './event';
 export { Group, Team } from './group';
 export { IdRefMap } from './relations';
 export { ScoreCard } from './scorecard';
